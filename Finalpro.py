5549faae294a2fbb5e88e9c73f152ab7e1b339d7
Adeganacc <100861893+Adeganacc@users.noreply.github.com>
2022-04-21 10:13:52 -0600

    Update Finalpro.py

--- Finalpro.py
+++ Finalpro.py
@@ -2,16 +2,18 @@
 
 from gpiozero import DistanceSensor, LED
 from time import sleep
+import subprocess
 
 sensor = DistanceSensor (echo=23, trigger=24, max_distance=2.0)
 led = LED(17)
 
 while True:
   distance = sensor.distance * 100
-  print("Distance : %.1f" % distance
-  sleep(1)
+  print("Distance : %.1f" % distance)
+  sleep(5)
   
   if distance <= 20:
+        subprocess.run(['echo date >> /home/$USER/sensorlog'])
         led.on()
   else:
         led.off()
