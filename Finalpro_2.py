9aea89a83f57b9b83e3002e7f69321dec96d511e
Adeganacc <100861893+Adeganacc@users.noreply.github.com>
2022-04-26 11:11:59 -0600

    Create Finalpro_2.py

---
+++ Finalpro_2.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+from gpiozero import DistanceSensor, LED
+from time import sleep
+
+sensor = DistanceSensor (echo=23, trigger=24, max_distance=2.0)
+red = LED(17)
+
+while True:
+  distance = sensor.distance * 100
+  print("Distance : %.1f" % distance)
+  sleep(1)
+  
+  if distance <= 20:
+        red.on()
+  else:
+        red.off()
