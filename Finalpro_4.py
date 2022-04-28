6ced7ec3f4db8d1b138a9d987f47ff20190d7b72
Adeganacc <100861893+Adeganacc@users.noreply.github.com>
2022-04-28 09:52:00 -0600

    Update Finalpro_4.py

--- Finalpro_4.py
+++ Finalpro_4.py
@@ -3,10 +3,17 @@
 from gpiozero import DistanceSensor, LED
 from time import sleep
 import subprocess
+import os.path
 
 sensor = DistanceSensor (echo=23, trigger=24, max_distance=2.0)
 red = LED(17)
 green = LED(27)
+
+if os.path.isFile('/home/$USER/sensorlog.txt') == False:
+  open('/home/$USER/sensorlog.txt')
+  
+  else:
+    pass
 
 while True:
   distance = sensor.distance * 100
